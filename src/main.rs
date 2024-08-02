ab8ea215a2515b226b9d5615afa9f799a846e887
x10102 <x10102.ot@gmail.com>
2024-08-02 16:14:50 +0200

    Some refactoring

--- src/main.rs
+++ src/main.rs
@@ -5,7 +5,7 @@ use sha1_smol::Sha1;
 use std::{
     collections::HashMap,
     env,
-    fs::{self, File, OpenOptions},
+    fs::{self, OpenOptions},
     io::{ErrorKind, Write},
     process::exit,
 };
@@ -13,7 +13,11 @@ use std::{
 const PATCH_DATA: &[u8] = include_bytes!("..\\res\\patches.bin");
 
 fn print_logo() {
-    println!(include_str!("..\\res\\banner.txt"), color_yellow=color_yellow, color_reset=color_reset);
+    println!(
+        include_str!("..\\res\\banner.txt"),
+        color_yellow = color_yellow,
+        color_reset = color_reset
+    );
 }
 
 #[derive(Deserialize)]
@@ -30,13 +34,42 @@ struct Version {
 }
 
 fn load_patches() -> HashMap<&'static str, Version> {
-    bincode::deserialize(PATCH_DATA).expect("Couldn't load embedded patches, program is probably corrupted.")
+    bincode::deserialize(PATCH_DATA)
+        .expect("Couldn't load embedded patches, program is probably corrupted.")
 }
 
-fn write_file(file: &mut File, buffer: &mut Vec<u8>) {
-    match file.write_all(buffer) {
-        Ok(_) => (),
-        Err(_) => fail("Unknown write error."),
+fn write_file(filename: &str, data: &mut Vec<u8>) {
+    let file = OpenOptions::new()
+        .create_new(true)
+        .write(true)
+        .open(filename);
+
+    match file {
+        Ok(mut file) => {
+            if file.write_all(data).is_err() {
+                fail("Unknown write error.")
+            }
+        }
+        Err(e) => match e.kind() {
+            ErrorKind::AlreadyExists => fail("File already exists."),
+            ErrorKind::PermissionDenied => {
+                fail("Insufficient permissions, make sure you have access to the file.")
+            }
+            _ => fail("Unknown I/O Error"),
+        },
+    }
+}
+
+fn read_file(filename: &str, buffer: &mut Vec<u8>) {
+    match fs::read(filename) {
+        Ok(mut data) => buffer.append(&mut data),
+        Err(e) => match e.kind() {
+            ErrorKind::NotFound => fail("File doesn't exist"),
+            ErrorKind::PermissionDenied => {
+                fail("Insufficient permissions, make sure you have access to the file.")
+            }
+            _ => fail("Unknown error while opening file"),
+        },
     }
 }
 
@@ -54,32 +87,33 @@ fn main() {
     }
     print_logo();
 
-    let original_filename = env::args().nth(1).unwrap();
-    let new_filename = env::args().nth(2).unwrap();
+    let args: Vec<String> = env::args().collect();
+    let [_, original_filename, new_filename] = args.try_into().unwrap();
     let mut file_data: Vec<u8> = Vec::new();
 
     println!("=> Reading {}...", original_filename);
 
-    match fs::read(&original_filename) {
-        Ok(data) => file_data = data,
-        Err(e) => match e.kind() {
-            ErrorKind::NotFound => fail("File doesn't exist"),
-            ErrorKind::PermissionDenied => fail("Insufficient permissions, make sure you have access to the file."),
-            _ => fail("Unknown error while opening file"),
-        },
-    }
+    read_file(&original_filename, &mut file_data);
 
     println!("=> Checking hash...");
-    let mut hash = Sha1::from(&file_data).hexdigest();
-    if !versions.contains_key(hash.as_str()) {
-        fail("Unknown version. Either your version is not supported or the file has been modified. Open an issue on GitHub.")
+    let hash = Sha1::from(&file_data).hexdigest();
+    let mut current_version = None;
+
+    match versions.get(hash.as_str()) {
+        Some(version) => current_version = Some(version),
+        None => fail("Unknown version. Either your version is not supported or the file has been modified. Open an issue on GitHub.")
     }
 
-    let current_version = versions.get(hash.as_str()).unwrap();
+    let current_version = current_version.unwrap();
+
     println!("=> Hash OK. Detected version: {}", current_version.name);
 
     for (idx, patch) in current_version.patches.iter().enumerate() {
-        println!("{color_yellow}=> Patching {} of {}...{color_reset}", idx + 1, current_version.patches.len());
+        println!(
+            "{color_yellow}=> Patching {} of {}...{color_reset}",
+            idx + 1,
+            current_version.patches.len()
+        );
         file_data.splice(
             patch.offset..patch.offset + patch.data.len(),
             patch.data.clone(),
@@ -87,7 +121,7 @@ fn main() {
     }
 
     println!("=> Checking hash...");
-    hash = Sha1::from(&file_data).hexdigest();
+    let hash = Sha1::from(&file_data).hexdigest();
     if current_version.final_hexdigest != hash.as_str() {
         fail("Unknown error. Open an issue on GitHub.");
     }
@@ -95,18 +129,7 @@ fn main() {
     println!("=> Hash OK.");
     println!("=> Writing patched file to {}...", new_filename);
 
-    let output_file = OpenOptions::new()
-        .create_new(true)
-        .write(true)
-        .open(new_filename);
+    write_file(&new_filename, &mut file_data);
 
-    match output_file {
-        Ok(mut file) => write_file(&mut file, &mut file_data),
-        Err(e) => match e.kind() {
-            ErrorKind::AlreadyExists => fail("File already exists."),
-            ErrorKind::PermissionDenied => fail("Insufficient permissions, make sure you have access to the file."),
-            _ => fail("Unknown I/O Error")
-        },
-    }
     println!("{color_green}Patcher done.{color_reset}");
 }
